010db5e23ab90c1dfe469230cbd8ef601790117a
jocsan06 <angeljocsan06@gmail.com>
2026-05-25 11:02:56 -0600

    Add files via upload

---
+++ demo_mysql_test.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "DROP TABLE customers"
+
+mycursor.execute(sql)
+
